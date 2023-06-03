55dea72a387e0ac9228ac55e23226b6f3524f47a
meow-meow-cpu <meowmeowcpu@gmail.com>
2023-06-03 12:34:49 -0400

    Added US01 and CukesRunner

---
+++ src/test/java/com/CRM/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.CRM.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/CRM/step_definitions",
+        dryRun = true,
+        tags = "",
+        publish = false
+
+)
+
+public class CukesRunner extends AbstractTestNGCucumberTests {
+}
